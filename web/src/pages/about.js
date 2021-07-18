0993c3e6702f827a869ed6e35cbc6ed9ba372918
jared james <jaredjames.personal@gmail.com>
2021-07-19 10:55:40 +1200

    added about

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -9,14 +9,13 @@ const About = () => (
     <div className={styles.container}>
       <div className={styles.container__title}><h1>Welcome!</h1></div>
       <div className={styles.container__about}>
-        <p>My name is Brie</p>
-        <p>lorem ipsum dolor sit amet</p>
-        <p>
-          lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum
-          dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
-          lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum
-          dolor sit amet lorem ipsum dolor sit amet
-        </p>
+        <p>Welcome, I’m Brie.</p>
+        <p>I am a finance and marketing student that will be working as a Financial Consultant next year at Ernst & Young</p>
+        <p>We are tested on money skills every single day, yet we were never taught these skills in school, nor do we really socialise about it. </p>
+        <p>I have started The Easy Breezy Investor to share financial information I have found helpful based on personal experiences.  </p>
+        <p>Your finances will always be part of your life, so why not learn a little about it. </p>
+        <p>It helped me and I hope it can help you too. </p>
+        <p>And just as Benjamin Franklin said, “an investment in knowledge pays the best interest”.</p>
       </div>
     </div>
   </Layout>
